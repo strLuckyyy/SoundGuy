b905f79a773c8ab393e9a87cf6ada4f17434ea03
defprofeta <abrahaofcon@gmail.com>
2023-11-24 16:32:30 -0300

    Mapa

--- SoundsGuy/Scripts/Entidades/Personagens/Player Scripts/PlayerMov.gd
+++ SoundsGuy/Scripts/Entidades/Personagens/Player Scripts/PlayerMov.gd
@@ -1,11 +1,44 @@
-extends Node
+extends CharacterBody2D
+
+var state_machine
+
+@export var speed = 30
+@export var friction = 0.4
+@export var accelerate = 0.4
+
+@export var tree: AnimationTree = null
+
+func ready():
+	state_machine = tree['parameters/playback']
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func move():
+	var getMove = Vector2(
+		Input.get_axis('left', 'right'),
+		Input.get_axis('up', 'down')
+	)
+	velocity = getMove * speed
+	
+	if getMove != Vector2.ZERO:
+		tree['parameters/move/blend_position'] = getMove
+		
+		velocity.x = lerp(velocity.x, getMove.normalized().x * speed, accelerate)
+		velocity.y = lerp(velocity.y, getMove.normalized().y * speed, accelerate)
+		return
+	
+	velocity.x = lerp(velocity.x, getMove.normalized().x * speed, friction)
+	velocity.x = lerp(velocity.y, getMove.normalized().y * speed, friction)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func animation() -> void:
+	ready()
+	if velocity.length() > 2:
+		state_machine.travel('move')
+		return
 	pass
+
+
+func _process(_delta):
+	move()
+	animation()
+	move_and_slide()
