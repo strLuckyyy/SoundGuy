567d23201137ac3d519dc44080618d8b9010cc6b
TheGamesModafoca <lucasframora@gmail.com>
2023-11-25 12:14:49 -0300

    0.7

--- SoundsGuy/Arte/Entidades/Inimigos/Morcego/bat.gd
+++ SoundsGuy/Arte/Entidades/Inimigos/Morcego/bat.gd
@@ -2,18 +2,20 @@ extends CharacterBody2D
 
 @onready var sprite = get_node("AnimatedSprite2D")
 
+var speed = 20
+var player_chase = false
+var motion = Vector2(0,0)
+@onready var player = $"../../../Personagens/Herus/CharacterBody2D"
+var character: PlayerChar
 
-
-
-func _physics_process(_delta):
-	move_and_slide()
+func _physics_process(delta: float) -> void:
 	_idle()
-
+	
 func _idle():
 	sprite.play("idle")
 
-
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("Herus"):
-		get_tree().queue_delete(self)
-	pass # Replace with function body.
+	if body.has_method("player"):
+		print("sim")
+		motion = motion.move_toward(body.position,speed)
+	pass
---
+++ SoundsGuy/Cenas/Fases/Herus.gd
@@ -0,0 +1,64 @@
+extends CharacterBody2D
+
+class_name PlayerChar
+
+func player():
+	pass
+
+@export var move_speed = 75
+
+@onready var sprite = get_node("AnimatedSprite2D")
+
+var input_direction: get = _get_input_direction
+var sprite_direction = "down": get = _get_sprite_direction
+@export var attacking = "no"
+var can_attack
+
+func _physics_process(_delta):
+	velocity = input_direction * move_speed
+	move_and_slide()
+	set_animation("idleDown")
+	
+func set_animation(animation):
+	if Input.is_action_just_pressed("attack") and attacking == "no":
+		sprite.play("attack")
+		attacking = "yes"
+		await get_tree().create_timer(0.55).timeout
+		_waveAttack()
+		attacking = "no"
+	if velocity != Vector2.ZERO and attacking ==  "no":
+		animation = "walking"
+		sprite.play("walk"+sprite_direction)
+	elif velocity == Vector2.ZERO and attacking == "no":
+		sprite.play("idle"+sprite_direction)
+
+func _get_input_direction():
+	var x = -int(Input.is_action_pressed("left")) + int(Input.is_action_pressed("right"))
+	var y = -int(Input.is_action_pressed("up")) + int(Input.is_action_pressed("down"))
+	input_direction = Vector2(x,y).normalized()
+	return input_direction
+
+func _get_sprite_direction():
+	match input_direction:
+		Vector2.LEFT:
+			sprite_direction = "Left"
+		Vector2.RIGHT:
+			sprite_direction = "Right"
+		Vector2.UP:
+			sprite_direction = "Up"
+		Vector2.DOWN:
+			sprite_direction = "Down"
+	return sprite_direction
+
+func _waveAttack():
+	$Wave/CollisionShape2D.disabled = false;
+	$Wave/AnimatedSprite2D.visible = true;
+	$Wave/AnimatedSprite2D.play("blast")
+	await get_tree().create_timer(0.29).timeout
+	$Wave/CollisionShape2D.disabled = true;
+	$Wave/AnimatedSprite2D.visible = false;
+
+func _on_wave_body_entered(body):
+	if body.is_in_group("Inimigos"):
+		body.queue_free()
+	pass
